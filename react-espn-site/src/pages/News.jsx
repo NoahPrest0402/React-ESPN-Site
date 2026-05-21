be8eadc6adb20d853727ee46023dc909a596b3ed
WiiStem <stempkawilliam3@gmail.com>
2026-05-21 12:55:06 -0400

    news

--- react-espn-site/src/pages/News.jsx
+++ react-espn-site/src/pages/News.jsx
@@ -1,29 +1,38 @@
 import { useState, useEffect } from "react";
-export default function News(){
+export default function News() {
+  //Const variable that can be changed
+  const [data, setData] = useState([]);
 
-      //Const variable that can be changed
-      const [data, setData] = useState();
-     
-      //Runs when component loads initially
-      useEffect(() => {
-        fetch(
-          `https://site.api.espn.com/apis/site/v2/sports/baseball/mlb/news`
-        )
-          .then((res) => res.json())
-          .then((res) => {
-            setData(res);
-          });
-      }, []);
-     
-      return (
-        <>
-          <h1>News</h1>
-          {data &&
-            <div>
-                {JSON.stringify(data)}
+  //Runs when component loads initially
+  useEffect(() => {
+    fetch(`https://site.api.espn.com/apis/site/v2/sports/baseball/mlb/news`)
+      .then((res) => res.json())
+      .then((res) => {
+        setData(res.articles);
+      });
+  }, []);
+
+  return (
+    <>
+      <div className="container">
+        <h1>ESPN MLB News</h1>
+        <div className="news-grid">
+          {data.map((article, index) => (
+            <div className="news-card" key={index}>
+              <img
+                className="news-image"
+                src={article.images?.[0]?.url}
+                alt={article.headline}
+              />
+              <div className="news-content">
+                <h2>{article.headline}</h2>
+                <p>{article.description}</p>
+                <a href={article.links.web.href}>Read More</a>
+              </div>
             </div>
-          }
-        </>
-      );
-
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
